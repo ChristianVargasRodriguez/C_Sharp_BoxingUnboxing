dfd8e2fc55d16e7a70f2e1faf13c669a09d9a9ef
Christian Vargas <christianj.vrgs@gmail.com>
2023-10-05 00:25:04 -0300

    Ejercicio con objetos Boxing Unboxing

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<object> miLista = new List<object>();
+
+miLista.Add(7);
+miLista.Add(28);
+miLista.Add(-1);
+miLista.Add(true);
+miLista.Add("chair");
+
+Console.WriteLine("Valores en la lista:");
+foreach (var item in miLista)
+{
+    Console.WriteLine(item);
+}
+
+int suma = 0;
+foreach (var item in miLista)
+{
+    if (item is int) // Verificar si el elemento es de tipo int.
+    {
+        suma += (int)item; // Sumar el valor si es de tipo int.
+    }
+}
+Console.WriteLine("Suma de los valores enteros: " + suma);
